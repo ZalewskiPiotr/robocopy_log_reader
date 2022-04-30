fff7b335a294913202115bbba33e73f77bd8c43f
ZalewskiPiotr <1piotrzalewski@gmail.com>
2022-04-30 19:46:40 +0200

    Dodano klasę do przechowywania informacji o logach dotyczących jednego wywołania polecenia 'robocopy'. Dodano tymczasowe (do testów) wywołanie klasy OneRobocopyInfo

--- robocopy_log_reader/runner.py
+++ robocopy_log_reader/runner.py
@@ -1,2 +1,7 @@
+from one_robocopy_info import OneRobocopyInfo
+
 if __name__ == "__main__":
+    # sekcja testowa - testujemy to co wytworzysliśmy
+    info = OneRobocopyInfo('24 kwieciec 2022', '25 wkiecień 2022', 'c:\dane\source', 'c:\dane\dest', 1, 2, 3, 10, 20, 30)
     print('hello')
+    print(info.__str__())
